2b5642b227c4008c3c1537b1dccd363b52e8df7e
shourya <ss02184@surrey.ac.uk>
2020-07-22 15:27:57 +0100

    created production databases

--- Gemfile
+++ Gemfile
@@ -7,6 +7,7 @@ ruby '2.6.6'
 gem 'rails', '~> 5.2.4', '>= 5.2.4.3'
 # Use sqlite3 as the database for Active Record
 gem 'sqlite3'
+gem 'pg'
 # Use Puma as the app server
 gem 'puma', '~> 3.11'
 # Use SCSS for stylesheets
--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2020_07_14_145815) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "sent_mails", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
